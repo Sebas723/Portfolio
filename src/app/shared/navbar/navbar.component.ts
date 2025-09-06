8c606c516867f0cd4ff6a68cf356f974a3c3276a
sebas723 <tairon723@gmail.com>
2025-09-06 15:22:46 -0500

    A refactoring of the navbar component is performed by applying some good practices

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Interfaces
+import { NavbarTemplate } from '../../models/NavbarTemplate';
+
 @Component({
   selector: 'app-navbar',
   imports: [CommonModule],
@@ -15,6 +18,25 @@ export class NavbarComponent {
 
   //Variable para controlar el estado del menú dropdown
   menuOpen = false;
+
+  //Navbar
+  navbarItems: NavbarTemplate[] = [
+    { 
+      navbarOptions: [
+        { id: 1, name: 'Inicio', sectionId: '' },
+        { id: 2, name: 'Experiencia', sectionId: '' },
+        { id: 3, name: 'Proyectos', sectionId: '' },
+        { id: 4, name: 'Acerca de mi', sectionId: '' },
+        { id: 5, name: 'Contacto', sectionId: '' }
+      ],
+      navbarIcons: [
+        { id: 1, icon: 'pi pi-linkedin', link: '' },
+        { id: 2, icon: 'pi pi-github', link: '' },
+        { id: 3, icon: 'pi pi-github', link: '' },
+        { id: 4, icon: 'pi pi-linkedin', link: '' },
+      ]
+    },
+  ];
 
   // Evento para abrir menú dropdown
   toggleMenu() {
