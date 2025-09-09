2023b8773b15c026734030e724822f84929ed44a
sebas723 <tairon723@gmail.com>
2025-09-08 22:29:21 -0500

    The structure and preliminary design for the about me section is done

---
+++ src/app/views/about-me/about-me.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+// Componentes
+import { ScrollArrowComponent } from "../../shared/scroll-arrow/scroll-arrow.component";
+
+@Component({
+  selector: 'app-about-me',
+  imports: [ScrollArrowComponent],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.css'
+})
+export class AboutMeComponent {
+
+}
